ffc9c7a30a999c624ce4a9690acaa98add5f37f6
artjerom <artjerom@gmail.com>
2016-12-23 18:14:55 +0300

    шаблоны

--- dev/js/app/LoanCalculatorView.js
+++ dev/js/app/LoanCalculatorView.js
@@ -117,6 +117,7 @@ var LoanCalculatorView = Backbone.View.extend({
                     'backgroundSize': ($(range[i]).val() - $(range[i]).attr('min')) * 100 / ($(range[i]).attr('max') - $(range[i]).attr('min')) + '% 100%'
                 });
             this.model.set(type, $(range[i]).val());
+            console.log($(range[i]).val());
         }
     },
 
